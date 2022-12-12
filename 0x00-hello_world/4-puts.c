41d48dfd207d54f33909f4d7b801380ad293d3e5
Harold-FN-2187 <laryeaharold319@gmail.com>
2022-12-12 20:58:50 +0000

    Dec 12-1.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-
+/** 
+ * main - Entry point
+ *
+ * Decription: 'Program using the puts() function'
+ *
+ * Return : Always 0 (Success)
+ */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
