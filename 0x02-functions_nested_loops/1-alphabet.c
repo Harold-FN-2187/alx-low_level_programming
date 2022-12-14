d6e36418584370e6616d4477c0ebd2cbf143e6f5
Harold-FN-2187 <laryeaharold319@gmail.com>
2022-12-14 22:32:38 +0000

    Dec14-2-2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include main.h
+
+/*
+ * main - entry point
+ *
+ * return: always zero (success)
+ */
+
+int main(void)
+{
+	char i;
+	for(i='a';i <= 'z'; i++)
+		_putchar(i)
+	_putchar(i);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * print_alphabet - declared in main.h
+ *
+ * description: to print alphabet in lowercase ten times
+ * Return : nothing is returned
+ */
+
+void print_alphaber_x10(void)
+{
+	char t;
+	int f;
+
+	for (f = 0;f <= 10; f++)
+	{
+		for(t = 'a';t <= 'z'; t++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
