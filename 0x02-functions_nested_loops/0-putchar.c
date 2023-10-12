617ba9f5106c5bda24fe1f0bcb841c9e568ac25b
Harold-FN-2187 <laryeaharold319@gmail.com>
2023-10-12 02:17:28 +0000

    made changes to 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _putchar - Writes a character to standard output
- * Returns: 1 (On success)
+ * @c : input integer
+ * Return: 1 always success
  */
 int _putchar(char c)
 {
