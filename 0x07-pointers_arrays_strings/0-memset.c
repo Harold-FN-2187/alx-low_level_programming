4df71b382dc3a040641a133c67e865e4ea06b63a
Harold-FN-2187 <laryeaharold319@gmail.com>
2023-10-24 02:27:02 +0000

    A few more changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,9 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int k;
 
 	for (k = 0; k < n; k++)
-		*(s + k) = b;
+	{
+		s[k] = b;
+	}
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,22 +8,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    char *pneedle;
-    char *bhaystack;
+char *pneedle;
+char *bhaystack;
+while (*haystack != '\0')
+{
+	bhaystack = haystack;
+	pneedle = needle;
 
-    while (*haystack != '\0')
+	while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 	{
-		bhaystack = haystack;
-		pneedle = needle;
-
-		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
-		{
-			haystack++;
-			pneedle++;
-		}
-		if (!*pneedle)
-			return (bhaystack);
-		haystack = bhaystack + 1;
+		haystack++;
+		pneedle++;
 	}
-	return (0);
+	if (!*pneedle)
+		return (bhaystack);
+	haystack = bhaystack + 1;
+}
+return (0);
 }
