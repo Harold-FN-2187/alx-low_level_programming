26f9f0baee064987978351605e6e31acd46df86f
Harold-FN-2187 <laryeaharold319@gmail.com>
2023-11-01 23:24:31 +0000

    0.01

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * main - multiply two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success, 1 if Error
+ */
+int main(int argc, char *argv[])
+{
+int n;
+int m;
+int diff;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+m = atoi(argv[2]);
+diff = n  *m;
+
+printf("%i\n", diff);
+return (0);
+}
+
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,35 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - add positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if ERROR, 0 if nothing is passed
+ */
+int main(int argc, char *argv[])
+{
+int f, g;
+int n = 0;
+
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (f = 1; f < argc; f++)
+{
+for (g = 0; argv[f][g] != '\0'; g++)
+{
+if (argv[f][g] < '0' || argv[f][g] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+n += atoi(argv[c]);
+}
+printf("%d\n", n);
+return (0);
+}
+
