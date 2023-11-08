df15bf2b17980a4c504319e06eaef7250843a943
Harold-FN-2187 <laryeaharold319@gmail.com>
2023-11-08 01:12:21 +0000

    0.03

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints name of file it was compiled from 
- * Returns : 0
+ * main  - prints name of file it was compiled from
+ * Return:  Returns 0
  */
-int main (void)
+int main(void)
 {
 printf("%s\n", __BASE_FILE__);
 return (0);
